7c0b26de8f7cdc3df650ed25fa76176780db57e2
Karthik.maddenapally <Karthik.maddenapally@netsmartz.net>
2023-03-08 04:32:47 +0800

    String Reverse

---
+++ src/test/java/assiangroup/programms/StringReverse.java
@@ -0,0 +1,13 @@
+package assiangroup.programms;
+
+public class StringReverse {
+
+    public static void main(String[] args){
+
+        String name = "Rajukumar Macha";
+        char[] resultArray = name.toCharArray();
+        for(int i=resultArray.length-1;i>=0;i--){
+                System.out.print(resultArray[i]);
+        }
+    }
+}
